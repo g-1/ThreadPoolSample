371b6f19bb31a531e2a01bd521f9cada59152e48
Takahiro Naka <naka.takahiro@gmail.com>
2016-10-24 23:07:02 +0900

    Initial Commit

---
+++ ThreadPoolSample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ThreadPoolSample
+//
+//  Created by タカ on 2016/10/24.
+//  Copyright © 2016年 タカ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
